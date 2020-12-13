05bb9665915f69df4ca2d8cc5168841a9a25f785
zero45 <65314554+zero45@users.noreply.github.com>
2020-12-13 20:55:28 +0000

    formatting improvements

--- js/main.js
+++ js/main.js
@@ -9,30 +9,30 @@ async function getGenMix() {
     const response = await fetch(api_url);
     const data = await response.json();
 
+
     let generationMix = data.data.generationmix
+
+    // date/time variables
     let dateFrom = data.data.from
     let parsedDateFrom = new Date(dateFrom)
     let DateStringFrom = parsedDateFrom.toDateString()
-    let TimeStringFrom = parsedDateFrom.toLocaleTimeString('en-UK')
+    let TimeStringFrom = (parsedDateFrom.toLocaleTimeString('en-UK')).slice(0, 4) + " PM"
 
     let dateTo = data.data.to
     let parsedDateTo = new Date(dateTo)
-    let TimeStringTo = parsedDateTo.toLocaleTimeString('en-UK')
+    let TimeStringTo = (parsedDateTo.toLocaleTimeString('en-UK')).slice(0, 4) + " PM"
 
     let timePara = document.createElement("P");
-    timePara.innerHTML = "This energy mix is true between " + TimeStringFrom + " and " + TimeStringTo + " Zulu Time on " + DateStringFrom;
+    timePara.innerHTML = "This energy mix was true from " + TimeStringFrom + " to " + TimeStringTo + " UTC on " + DateStringFrom;
     document.getElementById("myDiv1").appendChild(timePara);
 
-
+    // create map of json data
     for (let i = 0; i < generationMix.length; i++) {
 
         let fuel = data.data.generationmix[i].fuel
         let percentage = data.data.generationmix[i].perc
         generationData.set(fuel, percentage)
 
-        // let para = document.createElement("P");
-        // para.innerHTML = "The percentage mix of " + fuel + " is " + percentage + "%";
-        // document.getElementById("myDiv").appendChild(para);
     }
 
     // sort map
@@ -40,28 +40,41 @@ async function getGenMix() {
         yield* [...this.entries()].sort((a, b) => b[1] - a[1]);
     }
 
+    // title case function for chart labels
+    function toTitleCase(str) {
+        return str.replace(
+            /\w\S*/g,
+            function(txt) {
+                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            }
+        );
+    }
+
     // create separate arrays from map to pass to chart
     for (let [key, value] of generationData) {
-        energyLabels.push(key)
+        energyLabels.push(toTitleCase(key))
         generationMixData.push(value)
     }
 
-
+    // create chart
     let ctx = document.getElementById('myChart').getContext('2d');
     let myChart = new Chart(ctx, {
         type: 'pie',
         data: {
             datasets: [{
-
                 backgroundColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(249, 65, 68, 1)',
+                    'rgba(39, 125, 161, 1)',
+                    'rgba(243, 114, 44, 1)',
+                    'rgba(87, 117, 144, 1)',
+                    'rgba(248, 150, 30, 1)',
+                    'rgba(77, 144, 142, 1)',
+                    'rgba(249, 132, 74, 1)',
+                    'rgba(67, 170, 139, 1)',
+                    'rgba(249, 199, 79, 1)',
+                    'rgba(144, 190, 109, 1)'
                 ],
-                label: 'My First dataset',
+                label: 'UK Energy Mix',
                 // backgroundColor: 'rgb(255, 99, 132)',
                 // borderColor: 'rgb(255, 99, 132)',
                 data: generationMixData
@@ -70,13 +83,16 @@ async function getGenMix() {
         },
 
         options: {
-            responsive:true,
-            maintainAspectRatio: false
+            responsive: true,
+            maintainAspectRatio: false,
+
         }
     });
 
 }
 
+// create page
 getGenMix();
 
+// update every half an hour
 setInterval(getGenMix, 1800000);
