7c946538f4e8e68030d3ba762134da2448181422
zero45 <65314554+zero45@users.noreply.github.com>
2020-12-13 18:25:18 +0000

    added a pie chart

--- js/main.js
+++ js/main.js
@@ -1,41 +1,85 @@
+// import Chart from "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
+
 const api_url = 'https://api.carbonintensity.org.uk/generation';
+
+let energyLabels = []
+let generationMixData = []
+let generationData = new Map()
 
 async function getGenMix() {
     const response = await fetch(api_url);
     const data = await response.json();
-    console.log(data)
+
+    let generationMix = data.data.generationmix
     let dateFrom = data.data.from
     let parsedDateFrom = new Date(dateFrom)
     let DateStringFrom = parsedDateFrom.toDateString()
     let TimeStringFrom = parsedDateFrom.toLocaleTimeString('en-UK')
 
-
     let dateTo = data.data.to
     let parsedDateTo = new Date(dateTo)
-    let DateStringTo = parsedDateTo.toDateString()
     let TimeStringTo = parsedDateTo.toLocaleTimeString('en-UK')
-
-
-    console.log(parsedDateFrom.toLocaleTimeString('en-UK'))
-    console.log(parsedDateFrom.toDateString())
 
     let timePara = document.createElement("P");
     timePara.innerHTML = "This energy mix is true between " + TimeStringFrom + " and " + TimeStringTo + " Zulu Time on " + DateStringFrom;
     document.getElementById("myDiv1").appendChild(timePara);
 
-    for (let i = 0; i < data.data.generationmix.length; i++) {
 
+    for (let i = 0; i < generationMix.length; i++) {
 
         let fuel = data.data.generationmix[i].fuel
         let percentage = data.data.generationmix[i].perc
-        // console.log(fuel + " " + percentage)
+        console.log(fuel)
+        // energyLabels.push(fuel)
+        generationData.set(fuel, percentage)
+        // generationMixData.push(percentage)
         let para = document.createElement("P");
         para.innerHTML = "The percentage mix of " + fuel + " is " + percentage + "%";
         document.getElementById("myDiv").appendChild(para);
     }
 
+    console.log(generationData)
+
+    // sort map
+    generationData[Symbol.iterator] = function* () {
+        yield* [...this.entries()].sort((a, b) => b[1] - a[1]);
+    }
+    for (let [key, value] of generationData) {
+        energyLabels.push(key)
+        generationMixData.push(value)
+    }
+
+    console.log(energyLabels)
+    console.log(generationMixData)
+
+
+    let ctx = document.getElementById('myChart').getContext('2d');
+    let myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            datasets: [{
+                
+                backgroundColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                label: 'My First dataset',
+                // backgroundColor: 'rgb(255, 99, 132)',
+                // borderColor: 'rgb(255, 99, 132)',
+                data: generationMixData
+            }],
+            labels: energyLabels,
+        },
+
+        options: {}
+    });
+
 }
 
 getGenMix();
 
-// setInterval(getGenMix, 1000);
+setInterval(getGenMix, 1800000);
